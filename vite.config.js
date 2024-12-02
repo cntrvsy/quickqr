0be802299687befcdf5b822fdd9500e2ed0fd2fe
cntrvsy <benintangana@gmail.com>
2024-12-02 17:09:03 +0300

    changed the minSdkVersion to 26

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-//const host = '0.0.0.0'
+//// @ts-expect-error process is a nodejs global
+//const host = process.env.TAURI_DEV_HOST;
+const host = '0.0.0.0'
 
 
 // https://vitejs.dev/config/
