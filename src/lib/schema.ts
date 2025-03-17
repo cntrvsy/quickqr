1c23a0d10b0777a9e775858a1876ed797e386329
cntrvsy <benintangana@gmail.com>
2025-03-17 05:23:55 +0300

    updated and added more settings, svelte 5, tailwind css 4, updated shadcdn

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,8 +1,16 @@
 import { z } from 'zod';
 
+//types
+export type DownloadFormat = "svg" | "png" | "jpg" | "jpeg" | "webp";
+export type ErrorCorrectionLevel = "L" | "M" | "Q" | "H";
+
+
 // form schema
 export const urlSchema = z.object({
-    string: z.string().min(3)
+    url: z.string().min(3),
+    downloadFormat: z.string().nonempty(),
+    errorCorrectionLevel: z.string().nonempty(),
+    logoSize: z.number().min(1).max(25),
 })
 
 export type UrlSchema = typeof urlSchema
